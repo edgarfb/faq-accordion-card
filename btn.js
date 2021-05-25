6797adcc671cdfa4dc148ec17370d508b3f803e9
edgar <edgarfbritez@gmail.com>
2021-05-25 12:24:33 -0300

    I'm refactor the js file and html file and I changed clases on my stayles

--- btn.js
+++ btn.js
@@ -2,20 +2,21 @@
 
     let 
         d = document,
+        cardFaqBox = Array.from(d.getElementsByClassName('card-faq-box'));
         btn = Array.from(d.getElementsByClassName('btn')),
-        p = Array.from(d.getElementsByClassName('info-hidden'));
+        p = Array.from(d.getElementsByClassName('info-hidden')),
+        faqTitle = Array.from(d.getElementsByClassName('faq-title'));
     
-
-        console.log(btn);
-
-    btn.forEach(el => {
-        el.addEventListener('click', function btnClick() {
-            //It return the index of the btn clicked that will I use to show the <p> hidden
-            let i = btn.indexOf(el);
-            el.classList.toggle('btn-up');
+    cardFaqBox.forEach(function faqEvent(el) {
+        el.addEventListener('click', function faqClick() {
+            //It return the index of the cardFaqBox clicked that will I use to show the <p> hidden and switch the arrow up and down
+            let i = cardFaqBox.indexOf(el);
+            btn[i].classList.toggle('btn-up');
             p[i].classList.remove('p-hide');
             p[i].classList.toggle('info-show');
-        })
-    });
+            faqTitle[i].classList.toggle('title-selected');
+        });
+    })
+
     
 })()
