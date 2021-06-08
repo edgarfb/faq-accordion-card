ae72718597048c018c3e8e64c409f4a6da457fb0
edgar <edgarfbritez@gmail.com>
2021-06-08 08:28:00 -0300

    comment js file, change on styles

--- btn.js
+++ btn.js
@@ -10,10 +10,16 @@
     cardFaqBox.forEach(function faqEvent(el) {
         el.addEventListener('click', function faqClick() {
             //It return the index of the cardFaqBox clicked that will I use to show the <p> hidden and switch the arrow up and down
+        
             let i = cardFaqBox.indexOf(el);
+            // switch arrow up and down
             btn[i].classList.toggle('btn-up');
+
             p[i].classList.remove('p-hide');
+
             p[i].classList.toggle('info-show');
+            
+            // change color of h3 element
             faqTitle[i].classList.toggle('title-selected');
         });
     })
